175b0af2f7c46ebdb7401fc2470a2f55c3277e36
KevinIsMyName <wu.kevin.2520@gmail.com>
2023-10-17 22:56:53 -0400

    Use regex for streaks

--- src/events/chatCommands.js
+++ src/events/chatCommands.js
@@ -1,18 +1,27 @@
 const { Events } = require('discord.js');
-const { channelIds } = require('../../config.json');
+const { channelIds, prefix, regexArgs } = require('../../config.json');
 
 
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(interaction) {
 		// Only listen to a specific channel
-		if (channelIds.includes(interaction.channelId)) {
-			return;
-		}
+		if (!channelIds.includes(interaction.channelId)) return;
 
 		// Ignore reading bot's own messages
-		if (interaction.author.id === interaction.client.user.id) {
-			return;
+		if (interaction.author.id === interaction.client.user.id) return;
+
+		// Ignore messages that are not prefixed (this also catches non-text messages)
+		const messageContent = interaction.content;
+		if (messageContent.length < prefix.length && messageContent.substr(0, prefix.length)) return;
+
+		for (const args of regexArgs) {
+			const [ re, mode ] = args;
+			const pattern = new RegExp(re, mode);
+			if (messageContent.match(pattern)) {
+				// Tally streak for user
+				break;
+			}
 		}
 	},
 };
