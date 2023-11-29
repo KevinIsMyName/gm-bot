18dd087cd2346ac11d285a104f6b17e2ef771045
KevinIsMyName <36412364+KevinIsMyName@users.noreply.github.com>
2023-11-29 10:36:44 -0500

    Add more logging

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -24,7 +24,13 @@ class LoggerFactory {
 			transports: [
 				new winston.transports.File({ filename: `logs/${userService}-error.log`, level: 'error' }),
 				new winston.transports.File({ filename: `logs/${userService}.log`, level: 'debug' }),
+
+				new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+				new winston.transports.File({ filename: 'logs/warn.log', level: 'warning' }),
+				new winston.transports.File({ filename: 'logs/info.log', level: 'info' }),
+				new winston.transports.File({ filename: 'logs/debug.log', level: 'debug' }),
 				new winston.transports.File({ filename: 'logs/all.log', level: 'debug' }),
+
 				new winston.transports.Console({ format: winston.format.simple(), level: 'info' }),
 			],
 		});
